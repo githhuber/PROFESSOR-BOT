665c8eb7b2e99b0350ada73c93d263ed23fd2730
githhuber <86601101+githhuber@users.noreply.github.com>
2023-08-25 22:59:10 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -60,7 +60,7 @@ async def start(client, message):
         return
     if AUTH_CHANNEL and not await is_subscribed(client, message):
         try:
-            invite_link = await client.create_chat_invite_link(str(AUTH_CHANNEL))
+            invite_link = "https://t.me/+VwQIicjwYhAzNGRl"
         except ChatAdminRequired:
             logger.error("Make sure Bot is admin in Forcesub channel")
             return
