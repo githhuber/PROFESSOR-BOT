1d57f64f3e2582d260929ecb1ab64be709be2f56
githhuber <86601101+githhuber@users.noreply.github.com>
2023-08-28 10:23:11 +0530

    Update group_filter.py

--- plugins/group_filter.py
+++ plugins/group_filter.py
@@ -311,7 +311,7 @@ async def advantage_spell_chok(msg):
     g_s += await search_gagala(msg.text)
     gs_parsed = []
     if not g_s:
-        k = await msg.reply("I couldn't find any movie in that name.\n\nCheck your spelling at Gooogle 🤌 Or check if it's available on ott...🫠\\n If you can't find it! tag the admin with the movie name🗿")
+        k = await msg.reply("I couldn't find any movie in that name.\n\nCheck your spelling at Gooogle Or check if it's available on ott...🫠\n\n If you can't find it! tag the admin with the movie name🗿")
         await asyncio.sleep(8)
         await k.delete()
         return
@@ -340,7 +340,7 @@ async def advantage_spell_chok(msg):
     movielist += [(re.sub(r'(\-|\(|\)|_)', '', i, flags=re.IGNORECASE)).strip() for i in gs_parsed]
     movielist = list(dict.fromkeys(movielist))  # removing duplicates
     if not movielist:
-        k = await msg.reply("I couldn't find anything related to that.\n\nCheck your spelling at Gooogle 🤌 Or check if it's available on ott....🫠\n\n If you Can't find it! Tag the admin with the movie name🗿")
+        k = await msg.reply("I couldn't find anything related to that.\n\nCheck your spelling at Gooogle Or check if it's available on ott....🫠 \n\n If you Can't find it! Tag the admin with the movie name🗿")
         await asyncio.sleep(8)
         await k.delete()
         return
